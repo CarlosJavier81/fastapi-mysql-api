a3810beb36151a7406bb22d43808db9a37b32674
CarlosJavier81 <ingeniero.cfernandez@gmail.com>
2025-09-21 13:30:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def get_connection():
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        port=int(os.getenv("DB_PORT")),
+        port = int(os.getenv("DB_PORT", "3306")),
         database=os.getenv("DB_NAME")
     )
 
